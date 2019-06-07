a8a0b553c3911dc6699719b3aec094eda31723e4
KGorin <k.gorinskaya@mail.ru>
2019-06-07 13:38:47 +0000

    class? before threading

---
+++ robot.py
@@ -0,0 +1,5 @@
+class Dexter:
+    def __init__(self, min_dist = 100):
+        self.min_dist = min_dist # Minimal distance between Dexter and obstacle
+    def step():
+        self.forward()
--- robotA.py
+++ robotA.py
@@ -1,5 +1,4 @@
 import time
-import random
 from easygopigo3 import EasyGoPiGo3
 from picamera.array import PiRGBArray
 from picamera import PiCamera
@@ -7,9 +6,9 @@ import cv2
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-camera.resolution = (640, 480)
+camera.resolution = (352, 272)
 camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
+rawCapture = PiRGBArray(camera, size=(352, 272))
 
 # allow the camera to warmup
 time.sleep(0.1)
@@ -18,7 +17,7 @@ gpg = EasyGoPiGo3()
 my_distance_sensor = gpg.init_distance_sensor()
 
 curr_time = time.time()
-curr_speed = 290
+curr_speed = 320
 
 gpg.set_speed(curr_speed)
 
@@ -27,21 +26,17 @@ count = 0
 
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    gpg.forward()
+
+    key = cv2.waitKey(1) & 0xFF
+    
     image = frame.array
- 
-    # show the frame
     cv2.imshow("Frame", image)
- 
-    # clear the stream in preparation for the next frame
-    rawCapture.truncate(0)
+
+    rawCapture.truncate(0)  # clear the stream in preparation for the next frame
+    gpg.forward()
     dist = my_distance_sensor.read_mm()
 
-    if dist < 120 and dist > 99:
-        gpg.turn_degrees(45) # rotate around
-        print("Turning 45")
-        count+=1
-    elif dist < 100:
+    if dist < 100:
         gpg.turn_degrees(90) # rotate around
         print("Turning 90")
         count+=1
@@ -53,9 +48,8 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         print("Tired of turning...")
         count = 0
                
-    if (time.time() - curr_time) > 15 or cv2.waitKey(1) & 0xFF == ord("q"):     # turn off
+    if (time.time() - curr_time) > 15 or key == ord("q"):     # turn off
         gpg.stop()
         break
-
 
 gpg.stop()
