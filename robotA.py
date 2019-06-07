e88f59b767f259c1eef28a085459c8e698e7cac2
KGorin <k.gorinskaya@mail.ru>
2019-06-07 14:20:09 +0000

    this one is a thread test

--- robotA.py
+++ robotA.py
@@ -3,15 +3,19 @@ from easygopigo3 import EasyGoPiGo3
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import cv2
+import threading
 
-# initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-camera.resolution = (352, 272)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(352, 272))
-
-# allow the camera to warmup
-time.sleep(0.1)
+rawCapture = PiRGBArray(camera)
+    
+def taking_photo ():
+    print ("Smile!")
+    time.sleep(0.1)   # allow the camera to warmup
+    camera.resolution = (320, 240)
+    camera.capture(rawCapture, format="bgr")
+    image = rawCapture.array
+    cv2.imshow("Image", image)
+    rawCapture.truncate(0)
 
 gpg = EasyGoPiGo3()
 my_distance_sensor = gpg.init_distance_sensor()
@@ -19,24 +23,25 @@ my_distance_sensor = gpg.init_distance_sensor()
 curr_time = time.time()
 curr_speed = 320
 
-gpg.set_speed(curr_speed)
+#gpg.set_speed(curr_speed)
 
 time.sleep(1)
 count = 0
 
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+taking_photo ()
+while True:
 
     key = cv2.waitKey(1) & 0xFF
     
-    image = frame.array
-    cv2.imshow("Frame", image)
+   # gpg.forward()
+    
+    #dist = my_distance_sensor.read_mm()
 
-    rawCapture.truncate(0)  # clear the stream in preparation for the next frame
-    gpg.forward()
-    dist = my_distance_sensor.read_mm()
-
-    if dist < 100:
+    if key == ord("t"):
+        print ("here it comes...")
+        threading.Thread(target=taking_photo).start()
+    
+    '''if dist < 100:
         gpg.turn_degrees(90) # rotate around
         print("Turning 90")
         count+=1
@@ -46,10 +51,10 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     if count>3:
         gpg.turn_degrees(180) # rotate around
         print("Tired of turning...")
-        count = 0
+        count = 0'''
                
     if (time.time() - curr_time) > 15 or key == ord("q"):     # turn off
-        gpg.stop()
+        #gpg.stop()
         break
 
-gpg.stop()
+#gpg.stop()
