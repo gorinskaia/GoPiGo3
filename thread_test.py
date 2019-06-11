60e55bcb801a24664a87c01cde004630566dc307
KGorin <k.gorinskaya@mail.ru>
2019-06-11 09:36:46 +0000

    Thread works, fps is really good

--- thread_test.py
+++ thread_test.py
@@ -3,7 +3,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import cv2
 import threading
-import numpy as np
+from easygopigo3 import EasyGoPiGo3
     
 def taking_photo ():
     camera = PiCamera()
@@ -26,9 +26,36 @@ def taking_photo ():
             break
 
 
+gpg = EasyGoPiGo3()
+my_distance_sensor = gpg.init_distance_sensor()
+
+curr_time = time.time()
+curr_speed = 290
+
+gpg.set_speed(curr_speed)
+count = 0
+
 print ("Hello!")
 
 threading.Thread(target=taking_photo).start()
 
-#while True:
-    
+while True:
+    gpg.forward()
+    dist = my_distance_sensor.read_mm()
+
+    if dist < 100:
+        gpg.turn_degrees(90) # rotate around
+        print("Turning 90")
+        count+=1
+    else:
+        print("Distance Sensor Reading (mm): " + str(dist))
+        count = 0
+
+    if count>3:
+        gpg.turn_degrees(180) # rotate around
+        print("Tired of turning...")
+        count = 0
+               
+    if (time.time() - curr_time) > 20:     # turn off after 10 seconds
+        gpg.stop()
+        break
