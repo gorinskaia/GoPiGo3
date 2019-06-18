cd57b94e31c48256e74c20412de30eeda46cbb2c
KGorin <k.gorinskaya@mail.ru>
2019-06-18 14:02:27 +0200

    something like it

---
+++ RobotModel.py
@@ -0,0 +1,150 @@
+from panda3d.bullet import BulletWorld
+from panda3d.core import *
+from direct.task import Task
+import direct.directbase.DirectStart
+from panda3d.bullet import BulletPlaneShape
+from panda3d.bullet import BulletRigidBodyNode
+from panda3d.bullet import BulletBoxShape
+from panda3d.bullet import BulletVehicle
+from panda3d.bullet import ZUp
+
+class Robot (BulletVehicle):
+    def __init__ (self, render, world):
+        # Chassis body
+        shape = BulletBoxShape(Vec3(0.45, 0.98, 0.25))
+        ts = TransformState.makePos(Point3(0, 0, 0.06))
+
+        self.render = render
+        self.world = world
+        
+        chassisNP = render.attachNewNode(BulletRigidBodyNode('Vehicle'))
+        chassisNP.node().addShape(shape, ts)
+        chassisNP.setPos(0, 0, 1)
+        #chassisNP.setScale(1.5,1.5,1.5)
+        chassisNP.node().setMass(700.0)
+        chassisNP.node().setDeactivationEnabled(False)
+         
+        world.attachRigidBody(chassisNP.node())
+
+        robot = loader.loadModel("cube") # Static Robot model
+        robot.reparentTo (chassisNP) # Reparent the model to the node
+        robot_tex = loader.loadTexture("textures/robot.jpeg")
+        robot.setTexture(robot_tex, 1)
+
+        # Vehicle
+        super(Robot , self).__init__(world, chassisNP.node())
+        self.setCoordinateSystem(ZUp)
+        world.attachVehicle(self)
+        
+
+        # Wheel Right
+        wheelR = loader.loadModel('models/wheel.egg')
+        wheelR.reparentTo(render)
+        wheel_tex = loader.loadTexture("textures/red.jpg")
+        wheelR.setTexture(wheel_tex, 1)
+
+        self.addWheel(Point3(0.60, 0.95, 0.3), chassisNP)
+
+        # Wheel Left
+        wheelR = loader.loadModel('models/wheel.egg')
+        wheelR.reparentTo(render)
+        wheel_tex = loader.loadTexture("textures/red.jpg")
+        wheelR.setTexture(wheel_tex, 1)
+
+        self.addWheel(Point3(-0.60, 0.95, 0.3), chassisNP)
+
+        # Steering info
+        steering = 0.0            # degree
+        steeringClamp = 45.0      # degree
+        steeringIncrement = 120.0 # degree per second
+        
+    def addWheel(self, pos, np):
+        
+        wheel = self.createWheel()
+        
+        wheel.setNode(np.node())
+        wheel.setChassisConnectionPointCs(pos)
+        wheel.setFrontWheel(True)
+         
+        wheel.setWheelDirectionCs(Vec3(0, 0, -1))
+        wheel.setWheelAxleCs(Vec3(1, 0, 0))
+        wheel.setWheelRadius(0.33)
+        wheel.setMaxSuspensionTravelCm(40.0)
+         
+        wheel.setSuspensionStiffness(40.0)
+        wheel.setWheelsDampingRelaxation(2.3)
+        wheel.setWheelsDampingCompression(4.4)
+        wheel.setFrictionSlip(100.0)
+        wheel.setRollInfluence(0.1)
+
+
+    def getPosXYZ(self):
+        # gettingPos
+        position = self.getWheel(0).getNode()
+        try:
+            arrayOfStrings = str(position).split()
+            z = arrayOfStrings[5]
+            posXYZ = [float(arrayOfStrings[3]), float(arrayOfStrings[4]), float(z[:-1])]
+            return (posXYZ)
+        except IndexError:
+            return ([0.0 , 0.0 , -200.0])   
+
+
+    #function chor changin steering of front wheels
+    def setAngle(self, left, dt):
+        if(left):
+            self.steering += dt * self.steeringIncrement
+            self.steering = min(self.steering, self.steeringClamp)
+        else:
+            self.steering -= dt * self.steeringIncrement
+            self.steering = max(self.steering, -self.steeringClamp)
+        # Apply steering to front wheels
+        self.setSteeringValue(self.steering, 0)
+        self.setSteeringValue(self.steering, 1)
+
+    #function that sets engine force of front wheels
+    def setEngineForce(self, engineForce):
+        self.applyEngineForce(engineForce, 0)
+        self.applyEngineForce(engineForce, 1)
+
+    #function that sets brake
+    def setBrakeForce(self, brakeForce):
+        self.setBrake(brakeForce, 2)
+        self.setBrake(brakeForce, 3)
+
+
+     #function that returns current steering of front wheels
+    def getAngle(self):
+        return self.steering
+
+
+
+
+        # Apply steering to front wheels
+        #vehicle.setSteeringValue(steering, 0)
+        #vehicle.setSteeringValue(steering, 1)
+         
+        # Apply engine and brake to rear wheels
+        #vehicle.applyEngineForce(engineForce, 0)
+        #vehicle.applyEngineForce(engineForce, 3)
+        #vehicle.setBrake(brakeForce, 2)
+        #vehicle.setBrake(brakeForce, 3)
+
+
+
+        #print(vehicle.getNumWheels())
+        ###
+
+"""def update(task):
+    dt = globalClock.getDt()
+    world.doPhysics(dt)
+     
+    vehicle.setSteeringValue(steering, 0)
+    vehicle.applyEngineForce(engineForce, 0)
+
+    return task.cont
+
+
+taskMgr.add(update, 'update')
+
+run()"""
---
+++ main_simulation.py
@@ -0,0 +1,95 @@
+from RobotModel import Robot
+
+import sys
+import _thread as thread
+
+from direct.showbase.ShowBase import ShowBase
+
+from panda3d.core import *
+from panda3d.bullet import *
+from direct.task import Task
+
+
+class Simulation(ShowBase):
+    
+    def __init__(self):
+        
+        base.setBackgroundColor(0.1, 0.1, 0.8, 1)
+        base.setFrameRateMeter(True)
+        base.cam.setPos(0, -50, 0)
+        base.cam.lookAt(0, 0, 0)
+                
+        self.accept('escape', self.doExit)
+
+        taskMgr.add(self.update, 'updateWorld')
+
+        self.setup()
+
+    def doExit(self):
+        self.cleanup()
+        sys.exit(1)
+
+
+    def update(self, task):
+        dt = globalClock.getDt()
+
+        self.processInput(dt)
+        self.world.doPhysics(dt, 10, 0.008)
+        self.terrain.update()
+
+        return task.cont
+
+    
+    def setup(self):
+        self.worldNP = render.attachNewNode('World')
+        self.world = BulletWorld()
+        self.world.setGravity(Vec3(0, 0, -9.81))
+
+        self.controlVehicles = []
+        self.controlVehicles.append(Robot(self.worldNP, self.world))
+
+        # Make a plane
+        """shape = BulletPlaneShape(Vec3(0, 0, 1), 1)  # Collision shape
+        node = BulletRigidBodyNode('Ground')        # Create a rigid body
+        node.addShape(shape)                        # Add existing shape to it
+        np = render.attachNewNode(node)
+        np.setPos(0, 0, -2)
+        self.world.attachRigidBody(node)            # Attach the rigid body node to the world"""
+
+    def processInput(self, dt):
+        for vehicle in self.controlVehicles:
+            engineForce = 1000.0
+            brakeForce = 0.0
+
+            """ if inputState.isSet('forward'):
+                engineForce = 1000.0
+                brakeForce = 0.0
+
+            if inputState.isSet('backward'):
+                engineForce = -1000.0
+                brakeForce = 0.0
+
+            if inputState.isSet('reverse'):
+                engineForce = 0.0
+                brakeForce = 100.0
+
+            if inputState.isSet('left'):
+                vehicle.setAngle(True, dt)
+
+            if inputState.isSet('right'):
+                vehicle.setAngle(False, dt)"""
+
+    def cleanup(self):
+        self.world = None
+        self.worldNP.removeNode()
+
+    #function that navigates a vehicle to go in a square
+    def goSquare(self, vehicle, size):
+        while(True):
+            goTo([size, size], vehicle)
+            goTo([size, -size], vehicle)
+            goTo([-size, -size], vehicle)
+            goTo([-size, size], vehicle)
+
+sim = Simulation()
+base.run()
