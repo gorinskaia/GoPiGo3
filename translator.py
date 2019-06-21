840e60cc8dcd39d39e47c5b65fa72fe3b5638f63
KGorin <k.gorinskaya@mail.ru>
2019-06-21 15:46:59 +0200

    Add files via upload

--- translator.py
+++ translator.py
@@ -13,6 +13,10 @@ from panda3d.bullet import *
 COLLISION_DIST = 100
 SPEED = 300
 
+"""def start():
+    if ctrl = sim.ctrl:"""
+    
+
 while True:
     
     option = input ("Do you want to: A) Play simulation B) Control the robot. [a/b]? : ")
@@ -29,34 +33,33 @@ if option == "a":
     sim.sColl = sim.initCollisionSphere(sim.robot.robotModel, True, Point3(0,COLLISION_DIST/30,1))
     base.cTrav.addCollider(sim.sColl[0], sim.collHandEvent)
     sim.accept('into-' + sim.sColl[1], sim.collide)
-    
     robot = sim.robot
 
-    forward = ControllerForward(robot, 300, COLLISION_DIST)
-    turn90 = ControllerTurn(robot, 300, 90) #make another variable for sim.robot nd gopigo
-    sequence = [turn90, forward,turn90]
-        
-    sim.ctrl = ControllerSequence(robot, sequence)
-    sim.ctrl.start()
-
-    base.run()
 
 elif option == "b":
     import robotA
+
+    gpg = EasyGoPiGo3()
+    dexter = Dexter(gpg)
+
+# --- Your Code Goes Here ---
+
+forward = ControllerForward(robot, 300, COLLISION_DIST)
+turn90 = ControllerTurn(robot, 300, 90)
+sequence = [turn90, forward,turn90]
+
+# --- End Your Code ---
+
+if option == "a":
+
+    sim.ctrl = ControllerSequence(robot, sequence)
+    sim.ctrl.start()
+    base.run()
     
 
+
+elif option == "b":
+    import robotA
+
     
-    
-"""gpg = EasyGoPiGo3()
-dexter = Dexter(gpg)
-
-forward = ControllerForward(dexter, 350, 150)
-turn90 = ControllerTurn(dexter,350,90)
-
-sequenceSquare = [forward, turn90, forward, turn90, forward, turn90, forward]
-
-robot = ControllerSequence(dexter, sequenceSquare)
-
-robot.start()"""
-
 
