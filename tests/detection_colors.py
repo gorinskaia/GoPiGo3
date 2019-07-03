61216b0feb301f8b88cc83f1c199c671e0c8d961
KGorin <k.gorinskaya@mail.ru>
2019-07-03 12:50:37 +0200

    Add files via upload

--- tests/detection_colors.py
+++ tests/detection_colors.py
@@ -1,32 +1,31 @@
-# import the necessary packages
 import numpy as np
 import argparse
 import cv2
- 
-image = cv2.imread("target2.jpg")
 
-# define the list of boundaries
-boundaries = [( [1,20,100], [180,255,255] )]
+image = cv2.imread("results/res.jpg")
 hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 
-# loop over the boundaries
-for (lower, upper) in boundaries:
-# create NumPy arrays from the boundaries
-    lower = np.array(lower, dtype = "uint8")
-    upper = np.array(upper, dtype = "uint8")
+lower = np.array([80,0,65], dtype = "uint8")
+upper = np.array([120,140,160] , dtype = "uint8")
 
-    mask = cv2.inRange(hsv, lower, upper)
-    output = cv2.bitwise_and(hsv, image, mask = mask)
+mask = cv2.inRange(hsv, lower, upper)
+output = cv2.bitwise_and(hsv, image, mask = mask)
     
-    ret,thresh = cv2.threshold(gray_image,127,255,0)
-    M = cv2.moments(output)
-     
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
-     
-    cv2.circle(img, (cX, cY), 5, (255, 255, 255), -1)
+gray_image = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(gray_image,45,450,cv2.THRESH_BINARY)
 
-    #cv2.rectangle(output, (cX,0),(510,128), (0,255,0),3)
+cv2.imshow("output", gray_image)
+cv2.waitKey(0)
 
-    cv2.imshow("Images", np.hstack([image, output]))
-    cv2.waitKey(0)
+M = cv2.moments(thresh)
+
+cX = int(M["m10"] / M["m00"])
+cY = int(M["m01"] / M["m00"])
+         
+cv2.circle(image, (cX, cY), 5, (255, 255, 255), -1)
+cv2.putText(image, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
+
+    # Display Image
+
+cv2.imshow("Images", image)
+cv2.waitKey(0)
