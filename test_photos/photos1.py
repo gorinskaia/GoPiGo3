83119197bee87b40249fc6639a349ef778378adf
KGorin <k.gorinskaya@mail.ru>
2019-07-05 09:57:42 +0200

    better version
    erosion + area condition

--- test_photos/photos1.py
+++ test_photos/photos1.py
@@ -2,8 +2,13 @@ import numpy as np
 import argparse
 import cv2
 from matplotlib import pyplot as plt
+import time
+start_time = time.time()
 
-image = cv2.imread('photos/2.jpg')
+image = cv2.imread('photos/result.jpg')
+
+cv2.imshow("Images", image)
+cv2.waitKey(0)
 
 # Equalizing the histogramm
 def equalize_hist(img):
@@ -21,41 +26,39 @@ mask1 = cv2.inRange(hsv, (0,50,20), (5,255,255))
 mask2 = cv2.inRange(hsv, (175,50,20), (180,255,255))
 mask = cv2.bitwise_or(mask1, mask2 ) # Important
 
+cv2.imshow("output", mask)
+cv2.waitKey(0)
+
+kernel = np.ones((5,5),np.uint8)
+mask = cv2.erode(mask,kernel,iterations = 1)
+
+cv2.imshow("output", mask)
+cv2.waitKey(0)
+
 # Find the biggest red region
 (cnts, _) = cv2.findContours(mask.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-c = max(cnts, key = cv2.contourArea)
 
-res = np.mean(c, axis=0)
-res = res[0]
-cX = int(round(res[0]))
-cY = int(round(res[1]))
-
+cX = 320
+cY = 240
+      
+try:
+    for cnt in cnts:
+        if cv2.contourArea(cnt)<50: # Don't count the noise
+            pass
+        else:
+            c = max(cnts, key = cv2.contourArea)
+            res = np.mean(c, axis=0)
+            res = res[0]
+            cX = int(round(res[0]))
+            cY = int(round(res[1]))
+except ValueError:
+      cX = 320
+      cY = 240
+    
 cv2.circle(image, (cX, cY), 5, (255, 255, 255), -1)
 cv2.putText(image, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
 
-# Display Image
-cv2.imshow("Images", image)
+cv2.imshow("Result", image)
 cv2.waitKey(0)
-
-'''gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray_image,100,255,cv2.THRESH_BINARY)
-thresh = cv2.bitwise_not(thresh)
-
-cv2.imshow("output", thresh)
-cv2.waitKey(0)
-
-M = cv2.moments(thresh)
-
-cX = int(M["m10"] / M["m00"])
-cY = int(M["m01"] / M["m00"])
-
-print (cX, cY)
-         
-cv2.circle(image, (cX, cY), 5, (255, 255, 255), -1)
-cv2.putText(image, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
-
-    # Display Image
-
-cv2.imshow("Images", image)
-cv2.waitKey(0)
-'''
+        
+print("--- %s seconds ---" % (time.time() - start_time))
