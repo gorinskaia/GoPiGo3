036ed41bde203f871223a03ad8a5575a428bbd23
KGorin <k.gorinskaya@mail.ru>
2019-07-18 14:41:54 +0200

    not bad, honestly

--- tests/robot_perceptron.py
+++ tests/robot_perceptron.py
@@ -1,52 +1,61 @@
 from numpy import exp, array, random, dot
+import numpy
 
 
 class NeuronLayer():
     def __init__(self, number_of_neurons, number_of_inputs_per_neuron):
-        self.synaptic_weights = 2 * random.random((number_of_inputs_per_neuron, number_of_neurons)) - 1
-
+        self.weights = 2 * random.random((number_of_inputs_per_neuron, number_of_neurons)) - 1
 
 class NeuralNetwork():
     def __init__(self, layer1, layer2):
         self.layer1 = layer1
         self.layer2 = layer2
 
-    def __sigmoid(self, x):
+    def __sigmoid(self, x): #normalize 1-0
         return 1 / (1 + exp(-x))
 
-    def __sigmoid_derivative(self, x):
+    def __sigmoid_derivative(self, x): #gradient
         return x * (1 - x)
 
     def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
         for iteration in range(number_of_training_iterations):
 
-            output_from_layer_1, output_from_layer_2 = self.think(training_set_inputs)
+            output_layer_1, output_layer_2 = self.forward(training_set_inputs)
 
-            layer2_error = training_set_outputs - output_from_layer_2
-            layer2_delta = layer2_error * self.__sigmoid_derivative(output_from_layer_2)
+            layer2_error = training_set_outputs - output_layer_2
+            layer2_delta = layer2_error * self.__sigmoid_derivative(output_layer_2)
 
-            layer1_error = layer2_delta.dot(self.layer2.synaptic_weights.T)
-            layer1_delta = layer1_error * self.__sigmoid_derivative(output_from_layer_1)
+            layer1_error = layer2_delta.dot(self.layer2.weights.T)
+            layer1_delta = layer1_error * self.__sigmoid_derivative(output_layer_1)
 
             layer1_adjustment = training_set_inputs.T.dot(layer1_delta)
-            layer2_adjustment = output_from_layer_1.T.dot(layer2_delta)
+            layer2_adjustment = output_layer_1.T.dot(layer2_delta)
 
-            self.layer1.synaptic_weights += layer1_adjustment
-            self.layer2.synaptic_weights += layer2_adjustment
+            self.layer1.weights += layer1_adjustment
+            self.layer2.weights += layer2_adjustment
 
 
-    def think(self, inputs):
-        output_from_layer1 = self.__sigmoid(dot(inputs, self.layer1.synaptic_weights))
-        output_from_layer2 = self.__sigmoid(dot(output_from_layer1, self.layer2.synaptic_weights))
-        return output_from_layer1, output_from_layer2
+    def forward(self, inputs):
+        output_layer1 = self.__sigmoid(dot(inputs, self.layer1.weights))
+        output_layer2 = self.__sigmoid(dot(output_layer1, self.layer2.weights))
+        return output_layer1, output_layer2
 
     def print_weights(self):
         print ("    Layer 1: ")
-        print (self.layer1.synaptic_weights)
+        print (self.layer1.weights)
         print ("    Layer 2: ")
-        print (self.layer2.synaptic_weights)
+        print (self.layer2.weights)
+
+
+def normalize(arr):
+    _res = arr
+    for i in range(len(arr)):
+        _res[i][0] = (arr[i][0] - min(arr))/(max (arr) - min(arr))
+    return _res
 
 if __name__ == "__main__":
+
+    random.seed(1)
     
     # Create layer 1 (4 neurons, each with 1 inputs)
     layer1 = NeuronLayer(4, 1)
@@ -54,23 +63,26 @@ if __name__ == "__main__":
     # Create layer 2 (a single neuron with 4 inputs)
     layer2 = NeuronLayer(1, 4)
 
-    # Combine the layers to create a neural network
     neural_network = NeuralNetwork(layer1, layer2)
 
-    print ("Stage 1) Random starting synaptic weights: ")
-    neural_network.print_weights()
+    training_set_inputs = array([[15.0],[45.0], [60.0], [90.0],[1000.0]])
+    new_input = (80 - min(training_set_inputs))/(max (training_set_inputs) - min(training_set_inputs))
+    
+    training_set_inputs = normalize(training_set_inputs)
 
-    training_set_inputs = array([[45], [60], [75], [90], [105], [135], [150]])
-    training_set_outputs = array([[0, 0, 0.5, 0.8, 1, 1, 1]]).T
+    training_set_outputs = array([[ 0, 0, 0, 0.8, 1]]).T
 
-    #training_set_outputs = array([[0, 0, 150, 190, 200, 300, 300]]).T
+    neural_network.train(training_set_inputs, training_set_outputs, 10)
 
-    neural_network.train(training_set_inputs, training_set_outputs, 60000)
-
-    print ("Stage 2) New synaptic weights after training: ")
+    print ("New weights after training: ")
     neural_network.print_weights()
 
     # Test the neural network with a new situation.
-    print ("Stage 3) Considering a new situation -> ?: ")
-    hidden_state, output = neural_network.think(array([120]))
+    print ("Considering a new situation: ")
+    hidden_state, output = neural_network.forward(array([new_input]))
+    
     print (output)
+    _output = output*(max(training_set_outputs) - min(training_set_outputs)) + min(training_set_outputs)
+    print (_output)
+
+
